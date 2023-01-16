19cf67f3389235b9a29a193ebd7e00053e6d4043
Andrew Feng <bxandrew@gmail.com>
2023-01-16 17:16:23 -0500

    implement a iterative approach to max depth binary tree. This approach uses a stack-like structure to iterate through the entire tree and calculate the maxDepth as we iterate through the tree. Very similar to a BFS for a tree.

--- maxDepthBinaryTree.js
+++ maxDepthBinaryTree.js
@@ -88,6 +88,26 @@ var maxDepth = function(root, depth = 0) {
 //   return cache[root.val];
 // }
 
+// Attempting to do iterative solution to maxDepth
+// Use a stack to hold the current root and its current depth
+// Iterate through our entire tree
+
+const iMaxDepth = (root) => {
+  if (!root) return 0;
+  let maxDepth = 0;
+  const stack = [ [root, 1] ]; //start the stack off with our initial root element
+  //do a while loop here, while our stack has things inside it
+  while (stack.length) {
+    //destructure our root and depth out
+    const [currRoot, currDepth] = stack.pop();
+    if (currDepth > maxDepth) maxDepth = currDepth;
+    if (currRoot.left !== null) stack.push([currRoot.left, currDepth+1]);
+    if (currRoot.right !== null) stack.push([currRoot.right, currDepth+1]);
+  }
+
+  return maxDepth;
+}
+
 function TreeNode(val, left, right) {
   this.val = val === undefined ? 0 : val;
   this.left = left === undefined ? null : left;
@@ -106,4 +126,4 @@ const node3 = new TreeNode(3, node9, node20);
 
 // console.log(node3);
 
-console.log(maxDepth(node3));
+console.log(iMaxDepth(node3));
