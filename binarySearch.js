b625f6ec71f342b6b4e92e5c2e0dd0883d4990c0
Andrew Feng <bxandrew@gmail.com>
2023-01-22 16:25:02 -0500

    implement an iterative approach to binarySearch. Time complexity is still O(log(n)) because we split the array in half each time with our min and max pointers. Space complexity is upgraded to O(1) because we are now only using the same 3 variables for the pointers using a while loop instead of recursion.

--- binarySearch.js
+++ binarySearch.js
@@ -41,24 +41,50 @@
 // Time Complexity O(log(n)) -- Splits in half each time
 // Space Complexity O(log(n)) -- Have to make same number of variables as the times we split in half
 
-var search = function(nums, target, min = 0, max = nums.length-1) {
-  //calculate mid index based off min and max
-  let mid = Math.ceil((min + max) / 2) //round upwards
+// var search = function(nums, target, min = 0, max = nums.length-1) {
+//   //calculate mid index based off min and max
+//   let mid = Math.ceil((min + max) / 2) //round upwards
 
-  if (nums[mid] === target) return mid; //check if mid is target (basecase)
+//   if (nums[mid] === target) return mid; //check if mid is target (basecase)
 
-  if (min > max) return -1 //if min > max, we have finished searching through the array
+//   if (min > max) return -1 //if min > max, we have finished searching through the array
 
-  if (target > nums[mid]) {   // If target is greater than mid, it lies in right side of array
-    min = mid+1;
-    return search(nums, target, min, max);
-  } else if (target < nums[mid]) { // If target is less than mid, it lies in the left side of array
-    max = mid-1;
-    return search(nums, target, min, max);
+//   if (target > nums[mid]) {   // If target is greater than mid, it lies in right side of array
+//     min = mid+1;
+//     return search(nums, target, min, max);
+//   } else if (target < nums[mid]) { // If target is less than mid, it lies in the left side of array
+//     max = mid-1;
+//     return search(nums, target, min, max);
+//   }
+// };
+
+//----- Iterative Approach ----//
+// Time Complexity O(log(n)) -- Still splits the array in half each time using the min and max pointers
+// Space Complexity O(1) -- Only uses 3 variables to hold information, the min, max and mid pointers (if we dont count nums and target)
+
+const search = (nums, target) => {
+  let min = 0;
+  let max = nums.length - 1;
+
+  //loop until min > max
+  while (!(min > max)) {
+    // we want to redeclare mid every while loop so it gets a new mid value
+    let mid = Math.ceil( (min + max) / 2 );
+
+    if (nums[mid] === target) return mid;
+
+    if (target > nums[mid]) {
+      min = mid + 1;
+    } else if (target < nums[mid]) {
+      max = mid - 1;
+    }
+
   }
-};
+
+  return -1;
+}
 
 // Input: nums = [-1,0,3,5,9,12], target = 9
 // Output: 4
 
-console.log(search([-1,0,3,5,9,12,15,16], -1)); //5
+// console.log(search([-1,0,3,5,9,12,15,16], 16)); //5
