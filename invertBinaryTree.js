11e2ee9b17ca79acaff4cf469cd68b25800d650e
Andrew Feng <bxandrew@gmail.com>
2023-01-22 17:03:15 -0500

    implement an iterative (BFS) approach to invertBinaryTree. Time and Space complexity are both O(n) as we have to visit each node and the queue used in the iterative implementation will grow as we iterate through the tree. The iterative approach however uses an array to store the extra space whereas the recursive approach is limited to the callstack size because of the nature of recursion.

--- invertBinaryTree.js
+++ invertBinaryTree.js
@@ -31,25 +31,59 @@
 // Time Complexity: O(n) //we have to visit each node
 // Space Complexity: O(n) //A new set of variables is used each time we recurse through each node
 
-var invertTree = function(root) {
-  if (root === null) return root; // If given a null root, return out the null root (handles if we enter a null child)
+// var invertTree = function(root) {
+//   if (root === null) return root; // If given a null root, return out the null root (handles if we enter a null child)
 
-  // Basecase is if we reach a leaf (terminal node) return out root (handles leaf case)
-  if (root.left === null && root.right === null) return root; // If the root has no children, simply return the root out (head)
+//   // Basecase is if we reach a leaf (terminal node) return out root (handles leaf case)
+//   if (root.left === null && root.right === null) return root; // If the root has no children, simply return the root out (head)
 
-  // Starting at our root
-  // Swap algo left and right
-  let temp = root.left; // temp hold our left root before we swap
-  root.left = root.right; // set left root to right (invert it)
-  root.right = temp; // set right root to temp var (left root)
+//   // Starting at our root
+//   // Swap algo left and right
+//   let temp = root.left; // temp hold our left root before we swap
+//   root.left = root.right; // set left root to right (invert it)
+//   root.right = temp; // set right root to temp var (left root)
 
-  //once we have swapped, simply recurse into each root
-  invertTree(root.left);
-  invertTree(root.right);
+//   //once we have swapped, simply recurse into each root
+//   invertTree(root.left);
+//   invertTree(root.right);
 
-  // Once we come back here, we simply return our root (head) since we are done iterating through our tree
+//   // Once we come back here, we simply return our root (head) since we are done iterating through our tree
+//   return root
+// };
+
+
+//----- Iterative Approach -----//
+// Strategy: Use a queue to iterate through the tree (similar to BFS);
+
+// Time Complexity: O(n) because we have to visit each node and their children to swap them
+// Space Complexity: O(n) because we have to expand our queue as we visit each node and their children.
+// Difference between iterative and recursive is that the space used in recursio is in the callstack (limited by the callstack size)
+// Iterative approach will use an array as the extra space so it is not limited to the callstack's max size.
+
+const invertTree = (root) => {
+  if (root === null) return root; //edge case if root is a null object, return out the null object
+
+  const queue = [ root ] //start with root node as the first item in queue
+
+  //iterate through the tree while queue has items
+  while (queue.length) {
+    // Grab the item in queue
+    let currNode = queue.shift();
+    // Then swap its children
+    let temp = currNode.left;
+    currNode.left = currNode.right;
+    currNode.right = temp;
+
+    //Then add their children to the queue
+    if (currNode.left !== null) queue.push(currNode.left);
+    if (currNode.right !== null) queue.push(currNode.right);
+  }
+
+  // Return our inverted tree at the end
   return root
-};
+}
+
+
 
 //Let's make our tree first
 
