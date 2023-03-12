ec026c2361aaf7ecf03d1af269a359d200e64113
Andrew Feng <bxandrew@gmail.com>
2023-03-12 14:22:17 -0400

    implement O(n) time and space complexity solution for majorityElement.

---
+++ majorityElement.js
@@ -0,0 +1,49 @@
+// Given an array nums of size n, return the majority element.
+
+// The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+// Example 1:
+// Input: nums = [3,2,3]
+// Output: 3
+
+// Example 2:
+// Input: nums = [2,2,1,1,1,2,2]
+// Output: 2
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// Input: an array of numbers
+// Output: the majority element
+
+// Strategy:
+// Count all occurances of the nums in an object
+// Iterate through our object and then find the highest value
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+var majorityElement = function (nums) {
+  const numsCount = {};
+  let highest = -Infinity;
+  let highestElement;
+
+  // Collect counts of our elements
+  for (let num of nums) {
+    numsCount[num] = numsCount[num] + 1 || 1;
+  }
+
+  // Find the number that appeared the most times
+  for (let key in numsCount) {
+    if (numsCount[key] > highest) {
+      highest = numsCount[key];
+      highestElement = key;
+    }
+  }
+
+  return Number(highestElement);
+};
+
+console.log(majorityElement([2, 2, 1, 1, 1, 2, 2]));
