1033ba5fcb2eb04dc617c0cd672786c4f4305729
Andrew Feng <bxandrew@gmail.com>
2023-03-18 13:44:03 -0400

    implement a O(n) time and space complexity solution for largestComponent using a set to stored visited nodes and an array to hold the size of each component.

---
+++ largestComponent.js
@@ -0,0 +1,57 @@
+// Write a function, largestComponent, that takes in the adjacency list of an undirected graph. The function should return the size of the largest connected component in the graph.
+
+// Input: A graph in the form of an adjacency list
+// Output: A number representing the size of the largest graph
+
+// Strategy:
+// Iterate through each node in the graph to iterate through each components.
+// Do a dfs traversal and during the traversal increment a counter, if not visited (each visited node will increase this count)
+// Return this count at the end of the traversal and store it.
+// Return the largest count at the very end
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+const largestComponent = (graph) => {
+  const visited = new Set();
+  const counts = [];
+
+  for (let node in graph) {
+    // Goal is to return how big this node is
+    counts.push(traverse(graph, node, visited));
+  }
+
+  // Edge case is for if the graph itself is empty
+  if (!counts.length) return 0;
+
+  return Math.max(...counts);
+};
+
+const traverse = (graph, current, visited, count = 0) => {
+  // If we have already visited the node, return our count (do not increment as this is already visited)
+  if (visited.has(String(current))) return count;
+
+  // Add to our visited set and also increment our count of how big the component is
+  visited.add(String(current));
+  count += 1;
+
+  // Do the dfs traversal
+  for (let neighbor of graph[current]) {
+    count = traverse(graph, neighbor, visited, count);
+  }
+
+  // After finish iterating through the component, return how big it is (represented by count)
+  return count;
+};
+
+console.log(
+  largestComponent({
+    0: ["8", "1", "5"],
+    1: ["0"],
+    5: ["0", "8"],
+    8: ["0", "5"],
+    2: ["3", "4"],
+    3: ["2", "4"],
+    4: ["3", "2"],
+  })
+); // -> 4
