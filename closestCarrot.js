f097c76c59a677eebb35406a4abde7d2e336c15c
Andrew Feng <bxandrew@gmail.com>
2023-03-19 13:15:23 -0400

    implement O(r * c) time and space complexity since we have to visit each position in the grid (grid-graph). Implemented using BFS traversal with a queue data structure to help facilitate the bfs traversal.

---
+++ closestCarrot.js
@@ -0,0 +1,56 @@
+// Write a function, closestCarrot, that takes in a grid, a starting row, and a starting column. In the grid, 'X's are walls, 'O's are open spaces, and 'C's are carrots. The function should return a number representing the length of the shortest path from the starting position to a carrot. You may move up, down, left, or right, but cannot pass through walls (X). If there is no possible path to a carrot, then return -1.
+
+// Input: A grid (grid-graph)
+// Output: The distance to the closest carrot marked "C"
+
+// Strategy:
+// Since this is trying to find the closest carrot position to the starting position, we should use a BFS approach
+// Implement a BFS using a queue
+// The first time we encounter the carrot will mean it's the closest carrot, therefore immediately return the distance.
+// If we have finished our BFS traversal and we have not found a carrot, return -1
+// Establish a visited set to prevent infinite loops
+
+// Time Complexity: O(r * c) because worse case scenario, we have to traverse every node
+// Space Complexity: O(r * c) because worse case scenario, we have to store every node we visited
+
+const closestCarrot = (grid, startRow, startCol) => {
+  const visited = new Set();
+
+  const queue = [[startRow, startCol, 0]]; // [position, distance]
+  while (queue.length > 0) {
+    const [currRow, currCol, distance] = queue.shift();
+
+    // Check if inbounds
+    const rowInbounds = currRow >= 0 && currRow < grid.length;
+    const colInbounds = currCol >= 0 && currCol < grid[0].length;
+    if (!rowInbounds || !colInbounds) continue;
+
+    if (grid[currRow][currCol] === "X") continue; // If it is a wall, continue on
+
+    if (grid[currRow][currCol] === "C") return distance; // We have found our closest carrot
+
+    // Check if we have visited this position before
+    const position = currRow + "," + currCol;
+    if (visited.has(position)) continue;
+    visited.add(position);
+
+    // Add our neighbors into the queue
+    queue.push([currRow, currCol - 1, distance + 1]);
+    queue.push([currRow, currCol + 1, distance + 1]);
+    queue.push([currRow - 1, currCol, distance + 1]);
+    queue.push([currRow + 1, currCol, distance + 1]);
+  }
+
+  return -1;
+};
+
+const grid = [
+  ["O", "O", "O", "O", "O"],
+  ["O", "X", "O", "O", "O"],
+  ["O", "X", "X", "O", "O"],
+  ["O", "X", "C", "O", "O"],
+  ["O", "X", "X", "O", "O"],
+  ["C", "O", "O", "O", "O"],
+];
+
+console.log(closestCarrot(grid, 1, 2)); // -> 4
