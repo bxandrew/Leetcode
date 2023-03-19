a15c413a18853d32fae0f83f1ca282495a065b17
Andrew Feng <bxandrew@gmail.com>
2023-03-19 11:58:28 -0400

    implement refactor for islandCount. Using a position variable and a check for rowInbounds/colInbounds. Using a set to keep track of visited positions (nodes).

--- islandCount.js
+++ islandCount.js
@@ -13,47 +13,81 @@
 // Time Complexity: O(n^2) specifically O(rols * cols)
 // Space Complexity: O(n^2) because of recursively traversing through each position.
 
+// const islandCount = (grid) => {
+//   let islandCount = 0;
+//   for (let row = 0; row < grid.length; row++) {
+//     for (let col = 0; col < grid[row].length; col++) {
+//       if (grid[row][col] === "L") {
+//         // If it is unfound land, dfs through it to find all connected land positions and then also increment our count after our dfs
+//         traverse(grid, row, col);
+//         islandCount++;
+//       } else {
+//         // Mark the position visited as we iterate past
+//         grid[row][col] = "V";
+//       }
+//     }
+//   }
+
+//   return islandCount;
+// };
+
+// // Purpose of traverse is to find all connected L's and mark them visited
+// const traverse = (grid, row, col) => {
+//   // If out of range return out
+//   if (row >= grid.length || row < 0 || col >= grid[row].length || col < 0) {
+//     return;
+//   }
+
+//   if (grid[row][col] === "V") return; // If visited, return out to prevent infinite loop
+
+//   // If water, mark visited and return out.
+//   if (grid[row][col] === "W") {
+//     grid[row][col] = "V";
+//     return;
+//   }
+
+//   // Else mark the land as visited and iterate through
+//   grid[row][col] = "V";
+
+//   // Go through each neighbor and do a recursive call for each to find all lands connected
+//   traverse(grid, row, col - 1); // left
+//   traverse(grid, row, col + 1); // right
+//   traverse(grid, row - 1, col); // top
+//   traverse(grid, row + 1, col); // botoom
+// };
+
 const islandCount = (grid) => {
-  let islandCount = 0;
+  const visited = new Set();
+  let count = 0;
+
   for (let row = 0; row < grid.length; row++) {
-    for (let col = 0; col < grid[row].length; col++) {
-      if (grid[row][col] === "L") {
-        // If it is unfound land, dfs through it to find all connected land positions and then also increment our count after our dfs
-        traverse(grid, row, col);
-        islandCount++;
-      } else {
-        // Mark the position visited as we iterate past
-        grid[row][col] = "V";
+    for (let col = 0; col < grid[0].length; col++) {
+      if (explore(grid, row, col, visited) === true) {
+        count++;
       }
     }
   }
 
-  return islandCount;
+  return count;
 };
 
-// Purpose of traverse is to find all connected L's and mark them visited
-const traverse = (grid, row, col) => {
-  // If out of range return out
-  if (row >= grid.length || row < 0 || col >= grid[row].length || col < 0) {
-    return;
-  }
+const explore = (grid, row, col, visited) => {
+  const rowInbounds = row >= 0 && row < grid.length;
+  const colInbounds = col >= 0 && col < grid[0].length;
+  if (!rowInbounds || !colInbounds) return false;
 
-  if (grid[row][col] === "V") return; // If visited, return out to prevent infinite loop
+  if (grid[row][col] === "W") return false;
 
-  // If water, mark visited and return out.
-  if (grid[row][col] === "W") {
-    grid[row][col] = "V";
-    return;
-  }
+  const position = row + "," + col;
+  if (visited.has(position)) return false;
+  visited.add(position);
 
-  // Else mark the land as visited and iterate through
-  grid[row][col] = "V";
+  explore(grid, row, col - 1, visited);
+  explore(grid, row, col + 1, visited);
+  explore(grid, row - 1, col, visited);
+  explore(grid, row + 1, col, visited);
 
-  // Go through each neighbor and do a recursive call for each to find all lands connected
-  traverse(grid, row, col - 1); // left
-  traverse(grid, row, col + 1); // right
-  traverse(grid, row - 1, col); // top
-  traverse(grid, row + 1, col); // botoom
+  return true;
 };
 
 const grid = [
